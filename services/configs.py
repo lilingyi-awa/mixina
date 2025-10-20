88ee997bea05e2bbca47fd0a6378846a26183217
li_lingyi <centreis@xiaoice.vsinger.com._.local>
2025-10-20 16:50:09 +0800

    Dynamic config update (only for prompt and banning)

--- services/configs.py
+++ services/configs.py
@@ -1,6 +1,20 @@
 import json as __json
+import vetariasn as vt
+import asyncio
 
 with open("./config.json", "r", encoding="utf-8") as __f:
     config = __json.load(__f)
 
-del __f, __json
+del __f
+
+@vt.register_daemon()
+async def swap_config():
+    global config
+    await asyncio.sleep(10)
+    while True:
+        try:
+            with open("./config.json", "r", encoding="utf-8") as __f:
+                config = __json.load(__f)
+            await asyncio.sleep(10)
+        except Exception:
+            await asyncio.sleep(1)
