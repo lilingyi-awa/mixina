f70ddcd937095116d318e1cf5642f22d6f9eef40
li_lingyi <centreis@xiaoice.vsinger.com._.local>
2025-10-21 13:03:31 +0800

    Antifake

--- services/yhlink.py
+++ services/yhlink.py
@@ -1,9 +1,12 @@
 import aikframe as F
 import vetariasn as vt
 import fastapi
+from services.configs import config
 
 YH = F.YunhuActivityManager()
 
 @vt.http.post("/v1/yunhu-callback")
-async def callback(req: fastapi.Request):
+async def callback(req: fastapi.Request, antifake: str = ""):
+    if antifake != config["antifake_key"]:
+        return # Reject fake webhook
     await YH.receive_event(await req.json())
